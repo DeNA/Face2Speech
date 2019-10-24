ebe52c2fbca5de35981e6d5dc57f756efa54c43d
kentaro-tachiban <kentaro.tachibana@dena.com>
2019-10-24 19:49:39 +0900

    update index.md

--- mk_md.py
+++ mk_md.py
@@ -11,9 +11,18 @@ fp.close()
 print("# Audio samples")
 print("")
 
-print("| id in VGGFace2 | Speaker name | Gender  |  SYNTH-FACE | SYNTHE-SPEECH  |")
+print("| Id in VGGFace2 | Speaker name | Gender  |  SYNTH-FACE | SYNTHE-SPEECH  |")
 print("|---|---|---|---|---|")
-print(" id00017 |  Aaron Rodgers  | Male | <audio controls=\"controls\" > <source src=\"wav/SYNTH-FACE/test.wav\" autoplay/> Your browser does not support the audio element. </audio> | hoge |")
+for fname in fnames_list:
+    vggid, voxcelbif, spkname, gender = fname.rstrip().split()
+    vggid = vggid.rstrip()
+    spkname = spkname.rstrip()
+    gender = gender.rstrip()
+    spkname = re.sub("_", " ", spkname)
+    spkname = re.sub("\"", " ", spkname)
+
+    print(" {} | {}  | {} | <audio controls=\"controls\" > <source src=\"wav/SYNTH-FACE/{}.wav\" autoplay/> Your browser does not support the audio element. </audio> | <audio controls=\"controls\" > <source src=\"wav/SYNTH-FACE/{}.wav\" autoplay/> Your browser does not support the audio element. </audio> |".format(vggid, spkname, gender, spkname, spkname))
+
 '''
 for fname in fnames_list:
     vggid, voxcelbif, spkname, gender = fname.rstrip().split()
@@ -41,3 +50,5 @@ for fname in fnames_list:
 
 print("")
 '''
+print("* We can't provide face images on this page because of copyrights")
+print("* However, the face images were provided to subjects for evaluation.")
