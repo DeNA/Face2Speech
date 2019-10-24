343d806ade04f68c52b26fd62f1b10451009abf7
kentaro-tachiban <kentaro.tachibana@dena.com>
2019-10-24 20:10:08 +0900

    update index.md

--- mk_md.py
+++ mk_md.py
@@ -13,17 +13,17 @@ print("")
 print("* We can't provide face images on this page because of copyrights.")
 print("* However, the face images were provided to subjects in evaluation.")
 print("")
-print("| Id in VGGFace2 | Speaker name | Gender  |  SYNTH-FACE | SYNTHE-SPEECH  |")
+print("| Id in VoxCeleb2 | Speaker name | Gender  |  SYNTH-FACE | SYNTHE-SPEECH  |")
 print("|---|---|---|---|---|")
 for fname in fnames_list:
-    vggid, voxcelbif, spkname, gender = fname.rstrip().split()
+    voxcelebid, vggid, spkname, gender = fname.rstrip().split()
     vggid = vggid.rstrip()
     spkname = spkname.rstrip()
     gender = gender.rstrip()
     spkname = re.sub("_", " ", spkname)
     spkname = re.sub("\"", " ", spkname)
 
-    print("| {} | {} | {} | <audio controls=\"controls\" > <source src=\"wav/SYNTH-FACE/{}.wav\" autoplay/> Your browser does not support the audio element. </audio> | <audio controls=\"controls\" > <source src=\"wav/SYNTH-SPEECH/{}.wav\" autoplay/> Your browser does not support the audio element. </audio> |".format(vggid, spkname, gender, vggid, vggid))
+    print("| {} | {} | {} | <audio controls=\"controls\" > <source src=\"wav/SYNTH-FACE/{}.wav\" autoplay/> Your browser does not support the audio element. </audio> | <audio controls=\"controls\" > <source src=\"wav/SYNTH-SPEECH/{}.wav\" autoplay/> Your browser does not support the audio element. </audio> |".format(vggid, spkname, gender, voxcelebid, voxcelebid))
 
 '''
 for fname in fnames_list:
